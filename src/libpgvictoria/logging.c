c6b5dea4f9adbdba1f984fcbb2a750785ab03d42
sai pranav <rajasaipranav0@gmail.com>
2026-09-03 23:47:24 +0530

    [#59] Check the hex dump line before measuring it
    Signed-off-by: sai pranav <rajasaipranav0@gmail.com>

--- src/libpgvictoria/logging.c
+++ src/libpgvictoria/logging.c
@@ -453,6 +453,15 @@ retry:
                   }
                }
 
+               /* pgvictoria_append() returns its input on allocation
+                  failure, so l is still NULL if the first append failed. */
+               if (l == NULL)
+               {
+                  free(n);
+                  n = NULL;
+                  break;
+               }
+
                if (strlen(l) == LINE_LENGTH * 2)
                {
                   full_line = true;
@@ -461,8 +470,8 @@ retry:
                {
                   if (strlen(l) < LINE_LENGTH * 2)
                   {
-                     int chars_missing = (LINE_LENGTH * 2) - strlen(l);
-                     for (int i = 0; i < chars_missing; i++)
+                     size_t chars_missing = (LINE_LENGTH * 2) - strlen(l);
+                     for (size_t i = 0; i < chars_missing; i++)
                      {
                         l = pgvictoria_append_char(l, ' ');
                      }
