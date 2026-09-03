32dfc29574928dfa4944011e294f12746324a557
sai pranav <rajasaipranav0@gmail.com>
2026-09-03 23:47:57 +0530

    [#60] Check the realloc result in pgvictoria_memory_dynamic_append
    Signed-off-by: sai pranav <rajasaipranav0@gmail.com>

--- src/libpgvictoria/memory.c
+++ src/libpgvictoria/memory.c
@@ -125,6 +125,11 @@ pgvictoria_memory_dynamic_append(void* orig, size_t orig_size, void* append, siz
    {
       s = orig_size + append_size;
       d = realloc(orig, s);
+      if (d == NULL)
+      {
+         *new_size = orig_size;
+         return orig;
+      }
       memcpy(d + orig_size, append, append_size);
    }
    else
